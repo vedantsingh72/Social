95fdfac56c71c4ec11ccc3a7fbcd92949ecd02bf
Vedant Singh <vedantsingh0072@gmail.com>
2025-07-22 00:48:23 +0530

    Deploying Vite site

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/Post-project/", 
   plugins: [react()],
-  base: '/social-app/',
+ 
  
 });
